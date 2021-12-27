6d5757c83b5ba27f2c271d9db2205f31279bb943
up201907075 <up201907075@fc.up.pt>
2021-12-27 18:37:47 +0000

    Corrected mistakes

--- functions.c
+++ functions.c
@@ -12,138 +12,140 @@
 
 void printHelp()
 {
-    printf("Usage: download ftp://[<user>:<password>@]<host>/<url-path>\n");
+  printf("Usage: download ftp://[<user>:<password>@]<host>/<url-path>\n");
 }
 
 int parseCommandString(char *commandArg, char **serverName, char *serverIP, char **name, char **password, char **file_name)
 {
-    char *soup = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
-    (*serverName) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
-    (*name) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
-    (*password) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
-    (*file_name) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
-    memmove(soup, commandArg + 6, strlen(commandArg));
-    //parsing hostname
-    char *temp = memchr(soup, '/', strlen(soup));
-    printf("\nTemp created: %s\n", temp);
-    *serverName = memmove(*serverName, soup, strlen(soup) - strlen(temp));
-    // serverName = memchr(soup, '/', strlen(soup));
-    //parsing user
-    char *token = strtok(soup, ":");
-    if (strcmp(token, soup) == 0)
-    {
-        strcpy(*name, "anonymous");
-        strcpy(*password, "empty");
-        // name = "anonymous";
-        // password = "empty";
-    }
-    else
-    {
-        char *aux = memchr(soup, ':', strlen(soup));
-        memmove(*name, soup, strlen(soup) - strlen(aux));
-        //parsing password
-        char *aux2 = strchr(aux, '@');
-        memmove(*password, aux, strlen(aux) - strlen(aux2));
-        memmove(*password, *password + 1, strlen(*password));
-        //remove-se o 1o pq é :
-    }
-    //parsing serverIP and serverName
-    char *last = strrchr(token, '/');
-    last++;
-    if (last != NULL)
-    {
-        memmove(serverIP, last, strlen(last));
-        memmove(*file_name, temp + 1, strlen(temp));
-    }
-    else
-    {
-        strcpy(serverIP, "");
-        memmove(*file_name, temp, strlen(temp));
-    }
-    printf("Parsed command string.\n");
-    printf("Command Arg: %s\n ServerName: %s\n ServerIP: %s\n name: %s\n password: %s\n file_name: %s\n temp: %s\n", commandArg, *serverName, serverIP, *name, *password, *file_name, temp);
-    return 0;
+  char *soup = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
+  (*serverName) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
+  (*name) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
+  (*password) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
+  (*file_name) = (char *)malloc((strlen(commandArg) + 1) * sizeof(char));
+  memmove(soup, commandArg + 6, strlen(commandArg));
+  //parsing hostname
+  char *temp = memchr(soup, '/', strlen(soup));
+  //parsing user, password and serverName
+  char *token = memchr(soup, ':', strlen(soup));
+  if (strcmp(token, soup) == 0)
+  {
+    memmove(*serverName, soup, strlen(soup) - strlen(temp));
+    strcpy(*name, "anonymous");
+    strcpy(*password, "empty");
+    // name = "anonymous";
+    // password = "empty";
+  }
+  else
+  {
+    memmove(*serverName, soup, strlen(soup) - strlen(temp));
+    *serverName = memchr(*serverName, '@', strlen(*serverName));
+    memmove(*serverName, *serverName+1, strlen(*serverName));
+    printf("serverName: %s\n", *serverName);
+    char *aux = memchr(soup, ':', strlen(soup));
+    memmove(*name, soup, strlen(soup) - strlen(aux));
+    //parsing password
+    char *aux2 = strchr(aux, '@');
+    memmove(*password, aux, strlen(aux) - strlen(aux2));
+    memmove(*password, *password + 1, strlen(*password));
+    //remove-se o 1o pq é :
+  }
+  //parsing serverIP and file_name
+  char *last = strrchr(token, '/');
+  last++;
+  if (last != NULL)
+  {
+    memmove(serverIP, last, strlen(last));
+    memmove(*file_name, temp + 1, strlen(temp));
+  }
+  else
+  {
+    strcpy(serverIP, "");
+    memmove(*file_name, temp, strlen(temp));
+  }
+  printf("Parsed command string.\n");
+  printf("Command Arg: %s\n ServerName: %s\n ServerIP: %s\n name: %s\n password: %s\n file_name: %s\n temp: %s\n", commandArg, *serverName, serverIP, *name, *password, *file_name, temp);
+  return 0;
 }
 
 void getIPfromDNS(char *servername, char *serverIP)
 {
-    struct hostent *h;
-    if ((h = gethostbyname(servername)) == NULL)
-    {
-        herror("gethostbyname()");
-        exit(-1);
-    }
-    strcpy(serverIP, inet_ntoa(*((struct in_addr *)h->h_addr)));
+  struct hostent *h;
+  if ((h = gethostbyname(servername)) == NULL)
+  {
+    herror("gethostbyname()");
+    exit(-1);
+  }
+  strcpy(serverIP, inet_ntoa(*((struct in_addr *)h->h_addr)));
 }
 
 void downloadFile(char *serverIP, char *name, char *password, char *file)
 {
-    int res;
-    int controlSocket = establishControlConnection(serverIP);
-    loginControlConnection(controlSocket, name, password);
-    int dataSocket = establishDataConnection(serverIP, controlSocket);
-    off_t fileLen = askForFile(controlSocket, file);
-    char buf[fileLen];
-    waitForFileTransfer(controlSocket);
-    readFile(dataSocket, buf, fileLen);
-    saveFile(file, buf, fileLen);
+  int res;
+  int controlSocket = establishControlConnection(serverIP);
+  loginControlConnection(controlSocket, name, password);
+  int dataSocket = establishDataConnection(serverIP, controlSocket);
+  off_t fileLen = askForFile(controlSocket, file);
+  char buf[fileLen];
+  waitForFileTransfer(controlSocket);
+  readFile(dataSocket, buf, fileLen);
+  saveFile(file, buf, fileLen);
 
-    if (close(controlSocket) < 0)
-    {
-        perror("close()");
-        exit(-1);
-    }
-    if (close(dataSocket) < 0)
-    {
-        perror("close()");
-        exit(-1);
-    }
+  if (close(controlSocket) < 0)
+  {
+    perror("close()");
+    exit(-1);
+  }
+  if (close(dataSocket) < 0)
+  {
+    perror("close()");
+    exit(-1);
+  }
 }
 
 int establishConnection(char *serverIP, int port)
 {
-    int sockfd;
-    struct sockaddr_in server_addr;
+  int sockfd;
+  struct sockaddr_in server_addr;
 
-    /*server address handling*/
-    bzero((char *)&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(serverIP); /*32 bit Internet address network byte ordered*/
-    server_addr.sin_port = htons(port);                /*server TCP port must be network byte ordered */
+  /*server address handling*/
+  bzero((char *)&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(serverIP); /*32 bit Internet address network byte ordered*/
+  server_addr.sin_port = htons(port);                /*server TCP port must be network byte ordered */
 
-    /*open a TCP socket*/
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  /*open a TCP socket*/
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket()");
+    exit(-1);
+  }
+  /*connect to the server*/
+  if (connect(sockfd,
+    (struct sockaddr *)&server_addr,
+    sizeof(server_addr)) < 0)
     {
-        perror("socket()");
-        exit(-1);
-    }
-    /*connect to the server*/
-    if (connect(sockfd,
-                (struct sockaddr *)&server_addr,
-                sizeof(server_addr)) < 0)
-    {
-        perror("connect()");
-        exit(-1);
+      perror("connect()");
+      exit(-1);
     }
 
     return sockfd;
-}
+  }
 
-int establishControlConnection(char *serverIP)
-{
+  int establishControlConnection(char *serverIP)
+  {
     char *connectionAnswer = "220-Welcome to the University of Porto's mirror archive";
     int socket = establishConnection(serverIP, SERVER_PORT);
     int res = socketReadAndVerify(socket, connectionAnswer, strlen(connectionAnswer) - 1);
     if (res)
     {
-        fprintf(stderr, "Error establishing connection");
-        exit(-1);
+      fprintf(stderr, "Error establishing connection");
+      exit(-1);
     }
     return socket;
-}
+  }
 
-void loginControlConnection(int controlSocket, char *name, char *password)
-{
+  void loginControlConnection(int controlSocket, char *name, char *password)
+  {
     int res;
     char command[BUFFER_SIZE];
     char *usernameAnswer = "331 Please specify the password.";
@@ -154,47 +156,47 @@ void loginControlConnection(int controlSocket, char *name, char *password)
     res = socketReadAndVerify(controlSocket, usernameAnswer, strlen(usernameAnswer) - 1);
     if (res)
     {
-        fprintf(stderr, "Error providing the user");
-        exit(-1);
+      fprintf(stderr, "Error providing the user");
+      exit(-1);
     }
     snprintf(command, BUFFER_SIZE, "pass %s\n", password);
     socketWrite(controlSocket, command);
     res = socketReadAndVerify(controlSocket, passwordAnswer, strlen(passwordAnswer) - 1);
     if (res)
     {
-        fprintf(stderr, "Error providing the password");
-        exit(-1);
+      fprintf(stderr, "Error providing the password");
+      exit(-1);
     }
-}
+  }
 
-int establishDataConnection(char *serverIP, int controlSocket)
-{
+  int establishDataConnection(char *serverIP, int controlSocket)
+  {
     char answer[BUFFER_SIZE];
     socketWrite(controlSocket, "PASV\n");
     socketRead(controlSocket, answer, BUFFER_SIZE);
     int port = getPASVPort(answer);
     int dataSocket = establishConnection(serverIP, port);
     return dataSocket;
-}
+  }
 
-int getPASVPort(char *answer)
-{
+  int getPASVPort(char *answer)
+  {
     char s[4][20];
     int nb[6];
     if (strncmp(answer, "227", 3))
     {
-        fprintf(stderr, "PASV didn't return 227");
-        exit(-1);
+      fprintf(stderr, "PASV didn't return 227");
+      exit(-1);
     }
     sscanf(answer, "%s %s %s %s (%d,%d,%d,%d,%d,%d)",
-           s[0], s[1], s[2], s[3],
-           &nb[0], &nb[1], &nb[2], &nb[3], &nb[4], &nb[5]);
+    s[0], s[1], s[2], s[3],
+    &nb[0], &nb[1], &nb[2], &nb[3], &nb[4], &nb[5]);
 
     return nb[4] * 256 + nb[5];
-}
+  }
 
-off_t askForFile(int socket, char *file)
-{
+  off_t askForFile(int socket, char *file)
+  {
     int cmdSize = strlen(file) + 5 + 1 + 1;
     char command[cmdSize];
     int strSize = strlen(file) + BUFFER_SIZE;
@@ -210,76 +212,76 @@ off_t askForFile(int socket, char *file)
     snprintf(expectedAnswer, cmpSize, "%s%s", expectedAnswerPrefix, file);
     if (strncmp(answer, expectedAnswer, cmpSize - 1))
     {
-        fprintf(stderr, "Wrong RETR answer: %s", answer);
-        exit(-1);
+      fprintf(stderr, "Wrong RETR answer: %s", answer);
+      exit(-1);
     }
 
     off_t fileSize = getFileSize(answer, strlen(expectedAnswer) + 1);
     return fileSize;
-}
+  }
 
-off_t getFileSize(char *answer, int size)
-{
+  off_t getFileSize(char *answer, int size)
+  {
     off_t fileSize;
     sscanf(answer + size, "(%jd bytes).", &fileSize);
     return fileSize;
-}
+  }
 
-void waitForFileTransfer(int socket)
-{
+  void waitForFileTransfer(int socket)
+  {
     char *transferComplete = "226 Transfer complete.";
     int res = socketReadAndVerify(socket, transferComplete, strlen(transferComplete));
     if (res)
     {
-        fprintf(stderr, "Couldn't receive file");
-        exit(-1);
+      fprintf(stderr, "Couldn't receive file");
+      exit(-1);
     }
-}
+  }
 
-void readFile(int socket, char *buf, off_t len)
-{
+  void readFile(int socket, char *buf, off_t len)
+  {
     socketRead(socket, buf, len);
-}
+  }
 
-void saveFile(char *filename, char *buf, off_t len)
-{
+  void saveFile(char *filename, char *buf, off_t len)
+  {
     char *fileNameNoPath = strrchr(filename, '/');
     if(fileNameNoPath == NULL){
-        fileNameNoPath = filename;
+      fileNameNoPath = filename;
     }
     else{
-        fileNameNoPath += 1;
+      fileNameNoPath += 1;
     }
     int fd = open(fileNameNoPath, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if (fd == -1)
     {
-        perror("open()");
-        exit(-1);
+      perror("open()");
+      exit(-1);
     }
     int res = write(fd, buf, len);
     if (res == -1)
     {
-        perror("write()");
-        exit(-1);
+      perror("write()");
+      exit(-1);
     }
-}
+  }
 
-void socketWrite(int socket, char *toWrite)
-{
+  void socketWrite(int socket, char *toWrite)
+  {
     size_t bytes = write(socket, toWrite, strlen(toWrite));
     if (bytes > 0)
     {
-        // printf("Bytes escritos %ld\n", bytes);
+      // printf("Bytes escritos %ld\n", bytes);
     }
     else
     {
-        perror("write()");
-        exit(-1);
+      perror("write()");
+      exit(-1);
     }
-}
+  }
 
-void socketRead(int socket, char *toRead, int toReadSize)
-{
+  void socketRead(int socket, char *toRead, int toReadSize)
+  {
     ssize_t bytes = read(socket, toRead, toReadSize);
     printf("%s", toRead);
     if (bytes > 0)
@@ -287,18 +289,18 @@ void socketRead(int socket, char *toRead, int toReadSize)
     }
     else
     {
-        perror("read()");
-        exit(-1);
+      perror("read()");
+      exit(-1);
     }
-}
+  }
 
-int socketReadAndVerify(int socket, char *toVerify, int bytes)
-{
+  int socketReadAndVerify(int socket, char *toVerify, int bytes)
+  {
     char toRead[BUFFER_SIZE];
     socketRead(socket, toRead, BUFFER_SIZE);
     if (strncmp(toRead, toVerify, bytes))
     {
-        return -1;
+      return -1;
     }
     return 0;
-}
+  }
